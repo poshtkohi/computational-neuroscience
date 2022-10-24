0c618a2ae37f3f47451d57cb3807f8a0180b621f
Alireza Poshtkohi <poshtkohi@users.noreply.github.com>
2022-10-25 00:14:00 +0100

    Delete human-P2X-electrophysiology-in-microglia/src directory

--- human-P2X-electrophysiology-in-microglia/src/load_initial_conditions_from_file_hp2x4_total_current.m
+++
@@ -1,7 +0,0 @@
-%------Functions -------%
-function [K_new, num_added] = load_initial_conditions_from_file_hp2x4_total_current(K)
-    x0 = load('data/initial-guess-x0-hp2x4-total-current.dat'); % Loads non-zero initial conditions
-    num_added = length(x0);
-    K_new = [K; x0];
-end
-%--------------------%
